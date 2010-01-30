0b368d36b8341edf82f6a2e22bb68cd6a50cd35d
Jeppe Toustrup <jeppe@tenzer.dk>
2010-01-30 14:46:25 +0100

    Added check if ?sitemap is in the URL, and skips the rest of the file if it isn't

--- zenphoto-sitemap.php
+++ zenphoto-sitemap.php
@@ -1,8 +1,12 @@
 <?php
-/**
- */
 
 $plugin_description = 'Here goes some description about zenphoto-sitemap';
 $plugin_author = 'Jeppe Toustrup';
 $plugin_version = '2';
 $plugin_URL = 'http://tenzer.dk/open-source/zenphoto-sitemap';
+
+# Check if '?sitemap' is specified. If it isn't then we should stop execution of this script.
+if(!isset($_GET['sitemap'])) {
+	return;
+}
+
