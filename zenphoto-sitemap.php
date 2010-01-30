a39fae46ba1b17cc43eeb237ff9178f22469a525
Jeppe Toustrup <jeppe@tenzer.dk>
2010-01-30 12:39:08 +0100

    Initial set up of the repository

---
+++ zenphoto-sitemap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ */
+
+$plugin_description = 'Here goes some description about zenphoto-sitemap';
+$plugin_author = 'Jeppe Toustrup';
+$plugin_version = '2';
+$plugin_URL = 'http://tenzer.dk/open-source/zenphoto-sitemap';
