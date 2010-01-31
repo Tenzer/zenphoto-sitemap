2c8001e7a9c4bb161e52747ff8a472aa716bd921
Jeppe Toustrup <jeppe@tenzer.dk>
2010-01-31 12:18:05 +0100

    - Added PHPDoc information to the start of the file. - The script now outputs the correct content-type and XML headers.

--- zenphoto-sitemap.php
+++ zenphoto-sitemap.php
@@ -1,12 +1,34 @@
 <?php
 
+/**
+ * Generates a sitemap.org compatible XML file, for use with Google and other search engines.
+ *
+ * Renders the sitemap if a gallery page is called with "?sitemap" in the URL.
+ *
+ * @author Jeppe Toustrup (Tenzer)
+ * @version 2
+ * @package plugins
+ */
+
 $plugin_description = 'Here goes some description about zenphoto-sitemap';
-$plugin_author = 'Jeppe Toustrup';
+$plugin_author = 'Jeppe Toustrup (Tenzer)';
 $plugin_version = '2';
 $plugin_URL = 'http://tenzer.dk/open-source/zenphoto-sitemap';
 
-# Check if '?sitemap' is specified. If it isn't then we should stop execution of this script.
+// Check if '?sitemap' is specified. If it isn't then we should stop execution of this script.
 if(!isset($_GET['sitemap'])) {
 	return;
 }
 
+// Output content type and charset
+header('Content-Type: text/xml;charset=utf-8');
+
+// Output XML file headers
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+echo "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\" xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+
+// End off the <urlset> tag
+echo '</urlset>';
+
+// Stop execution of the rest of Zenphoto
+exit();
